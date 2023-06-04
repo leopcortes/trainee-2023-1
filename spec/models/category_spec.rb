ee2eb1222e6e86f2989f711557a7319f073fcb06
leopcortes <leo.pereira.cortes@gmail.com>
2023-06-04 15:42:32 -0300

    validating if category name is unique

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -8,6 +8,12 @@ RSpec.describe Category, type: :model do
   context "Validating name" do
     it "should be invalid if name nil" do
       expect(build(:category, name:nil)).to be_invalid
+      expect(build(:category, name:'')).to be_invalid
+    end
+
+    it "should be invalid if name repeated" do
+      create(:category, name:'Memes')
+      expect(build(:category, name:'Memes')).to be_invalid
     end
   end
   
