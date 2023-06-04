fd206a7c86ba7eafea89ace56f5bffff2e9c592c
leopcortes <leo.pereira.cortes@gmail.com>
2023-06-04 16:57:13 -0300

    Validations of Post model, failing tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe Post, type: :model do
       expect(build(:post)).to be_valid
     end
   end
+
+  context "Validating title" do
+    it "should be invalid if title nil" do
+      expect(build(:post, title:nil)).to be_invalid
+      expect(build(:post, title:'')).to be_invalid
+    end
+  end
+
+  context "Validating content" do
+    it "should be invalid if content nil" do
+      expect(build(:post, content:nil)).to be_invalid
+    end
+  end
 end
