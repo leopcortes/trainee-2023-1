0353625af865a04f25fa65206fbc9454af381380
leopcortes <leo.pereira.cortes@gmail.com>
2023-06-04 16:57:41 -0300

    Valitations of Post model, passing tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+    validates :title, presence: :true, uniqueness: :true
+    validates :content, presence: :true
+
     has_many :post_categories
     has_many :categories, through: :post_category
 end
