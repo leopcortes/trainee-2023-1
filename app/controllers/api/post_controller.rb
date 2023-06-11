a7e7642aefd127a83435c0f622ab88efaa6e4e73
leopcortes <leo.pereira.cortes@gmail.com>
2023-06-11 16:07:06 -0300

    post controller, create method, passing tests

--- app/controllers/api/post_controller.rb
+++ app/controllers/api/post_controller.rb
@@ -1,2 +1,15 @@
 class Api::PostController < ApplicationController
+    def create
+        post = Post.new(post_params)
+        post.save!
+        render json: post, status: :created #201
+    rescue StandardError => e
+      render json: e, status: :bad_request 
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :content)
+    end
 end
