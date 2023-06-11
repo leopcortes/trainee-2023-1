670d0921275d75ec2cb7a0e80e4401f976f4a0a3
leopcortes <leo.pereira.cortes@gmail.com>
2023-06-11 16:18:18 -0300

    post controller, delete method, passing tests

--- app/controllers/api/post_controller.rb
+++ app/controllers/api/post_controller.rb
@@ -15,6 +15,14 @@ class Api::PostController < ApplicationController
         render json: e, status: :not_found #404 ou :bad_request #400
     end
 
+    def delete
+        post = Post.find(params[:id])
+        post.destroy!
+        render json: post, status: :ok
+    rescue StandardError => e
+        render json: e, status: :bad_request 
+    end
+
     private
 
     def post_params
