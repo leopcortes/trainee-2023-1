f2378e7e3be3a59657cff7a3604289a7928c024a
leopcortes <leo.pereira.cortes@gmail.com>
2023-06-11 16:06:43 -0300

    post controller, create method, failing tests

--- spec/requests/api/post_spec.rb
+++ spec/requests/api/post_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Api::Posts", type: :request do
-  
+  describe "POST /create" do
+		let(:post_params) do
+			attributes_for(:post)
+		end
+		context "when params are ok" do
+			it "return http status created" do
+				post "/api/post/create", params:{post: post_params}
+				expect(response).to have_http_status(:created)
+			end
+		end
+		context "when params are nil" do
+			post_params = nil
+			it "return http status bad_request" do
+				post "/api/post/create", params:{post: post_params}
+				expect(response).to have_http_status(:bad_request)
+			end
+		end
+		context "when params are repeated" do
+			it "return http status bad_request" do
+				post "/api/post/create", params:{post: post_params}
+				post "/api/post/create", params:{post: post_params}
+				expect(response).to have_http_status(:bad_request)
+			end
+		end
+	end
 end
