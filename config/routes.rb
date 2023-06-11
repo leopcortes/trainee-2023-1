056a79bea71b953af4300d275ad39419a740f8f8
leopcortes <leo.pereira.cortes@gmail.com>
2023-06-10 21:32:01 -0300

    CRUD routes for all models

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,46 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace 'api' do
+    scope 'category' do
+      post 'create', to: 'category#create'
+      get 'index', to: 'category#index'
+      get 'show/:id', to: 'category#show'
+      patch 'update/:id', to: 'category#update'
+      delete 'delete/:id', to: 'category#delete'
+    end
+    
+    scope 'post' do
+      post 'create', to: 'post#create'
+      get 'index', to: 'post#index'
+      get 'show/:id', to: 'post#show'
+      patch 'update/:id', to: 'post#update'
+      delete 'delete/:id', to: 'post#delete'
+    end
+    
+    scope 'feedback' do
+      post 'create', to: 'feedback#create'
+      get 'index', to: 'feedback#index'
+      get 'show/:id', to: 'feedback#show'
+      patch 'update/:id', to: 'feedback#update'
+      delete 'delete/:id', to: 'feedback#delete'
+    end
+    
+    scope 'commentary' do
+      post 'create', to: 'commentary#create'
+      get 'index', to: 'commentary#index'
+      get 'show/:id', to: 'commentary#show'
+      patch 'update/:id', to: 'commentary#update'
+      delete 'delete/:id', to: 'commentary#delete'
+    end
+    
+    scope 'post_category' do
+      post 'create', to: 'post_category#create'
+      get 'index', to: 'post_category#index'
+      get 'show/:id', to: 'post_category#show'
+      patch 'update/:id', to: 'post_category#update'
+      delete 'delete/:id', to: 'post_category#delete'
+    end
+  end
 end
