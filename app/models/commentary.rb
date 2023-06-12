8c222a28e133b72fed66b7fca520deba5e117696
leopcortes <leo.pereira.cortes@gmail.com>
2023-06-12 20:35:31 -0300

    validation if commentary content is empty

--- app/models/commentary.rb
+++ app/models/commentary.rb
@@ -1,3 +1,5 @@
 class Commentary < ApplicationRecord
+  validates :content, presence: :true
+
   belongs_to :post
 end
--- spec/models/commentary_spec.rb
+++ spec/models/commentary_spec.rb
@@ -8,13 +8,20 @@ RSpec.describe Commentary, type: :model do
     end
   end
 
-  context "Testing post" do
+  context "Validating post" do
     it "should be invalid if nil" do
       expect(build(:commentary, post_id: nil)).to be_invalid
     end
-    
+
     it "should be invalid if id does not exists" do
       expect(build(:commentary, post_id: -1)).to be_invalid
+    end
+  end
+
+  context "Validating content" do
+    it "should be invalid if content nil" do
+      expect(build(:commentary, content:nil, post_id: post.id)).to be_invalid
+      expect(build(:commentary, content:'', post_id: post.id)).to be_invalid
     end
   end
 end
