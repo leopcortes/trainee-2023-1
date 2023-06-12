33596ef857176bc2331c1c60417f09e582ecb92a
leopcortes <leo.pereira.cortes@gmail.com>
2023-06-12 20:03:07 -0300

    separating post and category validations

--- spec/models/post_category_spec.rb
+++ spec/models/post_category_spec.rb
@@ -10,13 +10,23 @@ RSpec.describe PostCategory, type: :model do
     end
   end
 
-  context "Testing category" do
+  context "Validating category" do
     it "should be invalid if nil" do
-      expect(build(:post_category, category: nil, post_id: nil)).to be_invalid
+      expect(build(:post_category, category: nil)).to be_invalid
     end
     
     it "should be invalid if id does not exists" do
-      expect(build(:post_category, category_id: -1, post_id: post.id)).to be_invalid
+      expect(build(:post_category, category_id: -1)).to be_invalid
+    end
+  end
+  
+  context "Validating post" do
+    it "should be invalid if nil" do
+      expect(build(:post_category, post_id: nil)).to be_invalid
+    end
+    
+    it "should be invalid if id does not exists" do
+      expect(build(:post_category, post_id: -1)).to be_invalid
     end
   end
 
