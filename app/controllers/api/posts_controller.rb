8268123d963346818390d7c35647ee146a05587a
leopcortes <leo.pereira.cortes@gmail.com>
2023-06-13 20:32:12 -0300

    posts controller, update method, passing tests

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -14,6 +14,14 @@ class Api::PostsController < ApplicationController
         render json: e, status: :not_found
     end
 
+    def update 
+        post = Post.find(params[:id])
+        post.update!(post_params)
+        render json: post, status: :ok
+    rescue StandardError => e
+        render json: e, status: :not_found #404 ou :bad_request #400
+    end
+
     private
 
     def post_params
