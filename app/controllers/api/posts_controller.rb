0d78dd33b53e2e74a118bf2b2e5ae318d8606aa0
leopcortes <leo.pereira.cortes@gmail.com>
2023-06-13 20:26:55 -0300

    posts controller, show method, passing tests

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -7,6 +7,13 @@ class Api::PostsController < ApplicationController
       render json: e, status: :bad_request 
     end
 
+    def show
+        post = Post.find(params[:id])
+        render json: post, status: :ok
+    rescue StandardError => e
+        render json: e, status: :not_found
+    end
+
     private
 
     def post_params
