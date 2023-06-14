a4c1a84dd92a07cf83e2fe8671517868111db607
leopcortes <leo.pereira.cortes@gmail.com>
2023-06-13 22:51:14 -0300

    categories controller, create method, failing test

--- spec/requests/api/categories_spec.rb
+++ spec/requests/api/categories_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Api::Categories", type: :request do
-  
+    describe "POST /create" do
+		let(:category_params) do
+			attributes_for(:category)
+		end
+		context "when params are ok" do
+			it "return http status created" do
+				post "/api/categories/create", params:{category: category_params}
+				expect(response).to have_http_status(:created)
+			end
+		end
+		context "when params are nil" do
+			category_params = nil
+			it "return http status bad_request" do
+				post "/api/categories/create", params:{category: category_params}
+				expect(response).to have_http_status(:bad_request)
+			end
+		end
+		context "when params are repeated" do
+			it "return http status bad_request" do
+				post "/api/categories/create", params:{category: category_params}
+				post "/api/categories/create", params:{category: category_params}
+				expect(response).to have_http_status(:bad_request)
+			end
+		end
+	end
 end
