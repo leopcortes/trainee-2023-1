3b9b2295c8aafbc2b3fb4bb782805013e83b3491
leopcortes <leo.pereira.cortes@gmail.com>
2023-06-16 17:13:44 -0300

    post categories controller, create method

--- app/controllers/api/post_categories_controller.rb
+++ app/controllers/api/post_categories_controller.rb
@@ -1,2 +1,15 @@
 class Api::PostCategoriesController < ApplicationController
+    def create
+        post_category = PostCategory.new(post_category_params)
+        post_category.save!
+        render json: post_category, status: :created
+    rescue StandardError => e
+        render e, status: :bad_request 
+    end
+
+    private
+
+    def post_category_params
+        params.require(:post_category).permit(:post_id, :category_id)
+    end
 end
--- spec/requests/api/post_categories_spec.rb
+++ spec/requests/api/post_categories_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "Api::PostCategories", type: :request do
-  
+    describe "POST /create" do
+		let(:postagem) { create(:post) }
+		let(:categoria) { create(:category) }
+		let(:post_category_params) { 
+			{
+                post_id: postagem.id,
+				category_id: categoria.id
+			}
+		}
+		context 'params are ok' do
+			it 'return https status created' do
+				post "/api/post_categories/create", params:{post_category: post_category_params}
+				expect(response).to have_http_status(:created)
+			end
+		end
+		context "when params are nil" do
+			it "return http status bad_request" do
+				post_category_params = nil
+				post "/api/post_categories/create", params:{post_category: post_category_params}
+				expect(response).to have_http_status(:bad_request)
+			end
+		end
+		context "when params are repeated" do
+			it "return http status bad_request" do
+				post "/api/post_categories/create", params: post_category_params
+				post "/api/post_categories/create", params: post_category_params
+				expect(response).to have_http_status(:bad_request)
+			end
+		end
+	end
 end
