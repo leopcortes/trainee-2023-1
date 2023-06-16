0e4445d636eda2f17007546c6d3aa2c965cca979
leopcortes <leo.pereira.cortes@gmail.com>
2023-06-16 13:17:58 -0300

    commentaries controller, create method

--- app/controllers/api/commentaries_controller.rb
+++ app/controllers/api/commentaries_controller.rb
@@ -1,2 +1,15 @@
 class Api::CommentariesController < ApplicationController
+    def create
+        commentary = Commentary.new(commentary_params)
+        commentary.save!
+        render json: commentary, status: :created
+    rescue StandardError => e
+        render e, status: :bad_request 
+    end
+
+    private
+
+    def commentary_params
+        params.require(:commentary).permit(:content, :post_id)
+    end
 end
