86e0a888062d8b1acd0cfeb409cafb879eb3ea84
leopcortes <leo.pereira.cortes@gmail.com>
2023-06-16 13:10:28 -0300

    feedbacks controller, index method, failing tests

--- spec/requests/api/feedbacks_spec.rb
+++ spec/requests/api/feedbacks_spec.rb
@@ -31,6 +31,46 @@ RSpec.describe "Api::Feedbacks", type: :request do
 		end
 	end
 
+	describe "GET /index" do
+		before do 
+			create(:post, id: 1, title: "Postagem1", content: "aaaaa")
+			create(:post, id: 2, title: "Postagem2", content: "bbbbb")
+			create(:post, id: 3, title: "Postagem3", content: "ccccc")
+			create(:feedback, id: 1, like: false, post_id:1)
+			create(:feedback, id: 2, like: true, post_id:2)
+			create(:feedback, id: 3, like: false, post_id:3)
+		end
+		context 'when index return' do
+		  before do
+			get '/api/feedbacks/index'
+		  end
+		  it 'return http status ok' do
+			expect(response).to have_http_status(:ok)
+		  end
+		  it 'return a json' do
+			expect(response.content_type).to eq('application/json; charset=utf-8')
+		  end
+		  it 'return created instances' do
+			expect(JSON.parse(response.body)).to eq([{
+			  'id' => 1,
+			  'like' => false,
+			  'post_id' => 1
+			},
+			{
+			  'id' => 2,
+			  'like' => true,
+			  'post_id' => 2
+			},
+			{
+			  'id' => 3,
+			  'like' => false,
+			  'post_id' => 3
+			}
+			])
+		  end
+		end
+	  end
+
 	describe "GET /show/:id" do
 		let(:feedback) {create(:feedback)}
 		context "when id exist" do
