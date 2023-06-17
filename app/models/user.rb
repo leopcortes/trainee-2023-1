e0282e928bec9e02970aac2a9806a460795479a0
leopcortes <leo.pereira.cortes@gmail.com>
2023-06-17 18:09:24 -0300

    user model tests and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
 
   acts_as_token_authenticatable
 
+  validates :name, presence: true, length: { minimum: 3, maximum: 30 }
+  validates :email, presence: true, uniqueness: true
+  validates :is_admin, inclusion: [true, false], exclusion: [nil]
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
