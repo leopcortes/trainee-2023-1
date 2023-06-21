8b7ba028df03de73b6d75aa4d74c2e39a0806408
leopcortes <leo.pereira.cortes@gmail.com>
2023-06-21 19:56:35 -0300

    creating users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+User.create(name: "Joao", email: "joao@gmail.com", password: "12345678", is_admin: true)
 Post.create(title: "Tutorial como subir de elo no lol", content: "Desista")
 Category.create(name: "Jogos", description: "Valorant/LOL/COD/Fortnite/Minecraft/etc")
 Feedback.create(like: false, post_id: 1)
@@ -13,6 +14,7 @@ Feedback.create(like: true, post_id: 1)
 Commentary.create(content: ":(", post_id: 1)
 Commentary.create(content: "Hahaha", post_id: 1)
 
+User.create(name: "Maria", email: "maria@gmail.com", password: "12345678", is_admin: true)
 Post.create(title: "Aonde assistir homem aranha online", content: "TV Box")
 Category.create(name: "TV", description: "Filmes/Series/Animes/Streamings/etc")
 Feedback.create(like: false, post_id: 2)
@@ -21,6 +23,7 @@ Commentary.create(content: "Que pena", post_id: 2)
 Commentary.create(content: "Legal", post_id: 2)
 
 
+User.create(name: "Pedro", email: "pedro@gmail.com", password: "12345678", is_admin: false)
 Post.create(title: "O que e um pontinho azul no ceu", content: "Urublue")
 Category.create(name: "Memes", description: "Piadas/Humor/Risos")
 Feedback.create(like: false, post_id: 3)
